3895c3e934abb9f2edce0fbd9dfeb59667b4acf3
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-09-29 20:17:57 +0300

    Basic flask app updated

--- tasktrackerapp/__init__.py
+++ tasktrackerapp/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 def create_app(): 
     app = Flask(__name__)
     title = "Task tracker LP26 project"
-    message = "Project is still in progress"
+    app_message = "Project is still in progress"
     app.config.from_pyfile('config.py')    
     @app.route('/')
     def index(): 
-        return render_template('index.html', title=title, message=message)
+        return render_template('index.html', title=title, message=app_message)
     return app
 
