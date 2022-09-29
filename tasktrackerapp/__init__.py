749a4a680a5fd0913a646d5fa8917ee43d073c79
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-09-29 21:13:56 +0300

    cofig added

--- tasktrackerapp/__init__.py
+++ tasktrackerapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 def create_app(): 
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
     app_title = "Task tracker LP26 project"
     app_message = "Project is still in progress"
     app.config.from_pyfile('config.py')    
