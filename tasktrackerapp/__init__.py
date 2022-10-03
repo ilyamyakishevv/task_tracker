b9489468ca0243b4ac6811452f748813a212fc07
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-10-03 20:05:55 +0300

    Updated with using flake8

--- tasktrackerapp/__init__.py
+++ tasktrackerapp/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 
-def create_app(): 
+
+def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     app_title = "Task tracker LP26 project"
-    app_message = "Project is still in progress"
-    app.config.from_pyfile('config.py')    
-    @app.route('/')
-    def index(): 
-        return render_template('index.html', title=app_title, message=app_message)
-    return app
+    message = "Project is still in progress"
+    app.config.from_pyfile('config.py')
 
+    @app.route('/')
+    def index():
+        return render_template('index.html', title=app_title, message=message)
+    return app
--- tasktrackerapp/models.py
+++ tasktrackerapp/models.py
@@ -1,14 +1,14 @@
-from tkinter.tix import COLUMN
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Tasks(db.Model): 
+
+class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String, nullable=False)
     desription = db.Column(db.String, nullable=False)
     status = db.Column(db.String, nullable=False)
     deadline = db.Column(db.Datetime, nullable=True)
 
-    def __repr__(self): 
+    def __repr__(self):
         return f"Task {self.id} {self.name} in status {self.status}"
