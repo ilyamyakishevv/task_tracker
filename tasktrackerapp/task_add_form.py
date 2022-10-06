e697e91a4bd9a3b4f7639d2ff68cf3ac7a820c02
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-10-07 02:15:35 +0300

    Form field updated for description field

--- tasktrackerapp/task_add_form.py
+++ tasktrackerapp/task_add_form.py
@@ -1,12 +1,12 @@
 from pydoc import render_doc
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateTimeField
+from wtforms import StringField, SubmitField, DateField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class TaskAdd(FlaskForm):
     name = StringField("Название задачи", validators=[DataRequired()], render_kw={'class':'form-control'})
-    description = StringField("Описание задачи", validators=[DataRequired()], render_kw={'class':'form-control'})
-    #status = StringField(default='IN WORK', render_kw={'class':'form-control'})
+    description = TextAreaField("Описание задачи", validators=[DataRequired()], render_kw={'class':'form-control'})
+    status = StringField(default='OPEN', render_kw={'class':'form-control'})
     deadline = StringField("Срок исполнения до", validators=[DataRequired()], render_kw={'class':'form-control'})
     submit = SubmitField("Создать задачу", render_kw={'class':'btn btn-primary'})
