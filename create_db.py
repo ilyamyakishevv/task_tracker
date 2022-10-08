33080fe10757ce6a455fedbd8d169bfdbb7f42b8
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-10-08 11:43:02 +0300

    Conflict resolve, flake8 update

--- create_db.py
+++ create_db.py
@@ -2,9 +2,5 @@ from tasktrackerapp import db, create_app
 
 app = create_app()
 
-with app.app_context(): 
-<<<<<<< HEAD
+with app.app_context():
     db.create_all()
-=======
-    db.create_all()
->>>>>>> a23f2ae09310f68ed3f07685ac9ff3418b4fffaf
