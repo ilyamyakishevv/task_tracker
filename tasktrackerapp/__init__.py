12599caab99c943b884291f61178891160a2804b
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-10-08 11:38:16 +0300

    Uptated with to date local project

--- tasktrackerapp/__init__.py
+++ tasktrackerapp/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
-from tasktrackerapp.models import db
+from crypt import methods
+from typing import DefaultDict
+from xxlimited import new
+from flask import Flask, render_template, flash, redirect, url_for
+from tasktrackerapp.models import Tasks, db
+from tasktrackerapp.task_add_form import TaskAdd
 
 
 def create_app():
@@ -12,4 +16,28 @@ def create_app():
     @app.route('/')
     def index():
         return render_template('index.html', title=app_title, message=message)
-    return app
+       
+    @app.route('/add_task')
+    def add_task():
+        title = "Добавить задачу"
+        add_task_form = TaskAdd()
+        return render_template('add_task.html', title=title, form=add_task_form)
+
+    @app.route('/adding_task', methods=['POST'])
+    def adding_task():
+        adding_form = TaskAdd() 
+        if adding_form.validate_on_submit(): 
+            task_name = adding_form.name.data
+            task_description = adding_form.description.data
+            task_deadline = adding_form.deadline.data
+            new_task = Tasks(name=task_name, description = task_description, status='OPEN', deadline = task_deadline)
+            db.session.add(new_task)
+            db.session.commit()
+            flash ("Задание успешно добавлено")
+            return redirect(url_for('add_task'))
+        flash ("Заполните все поля!")
+        return redirect(url_for('add_task'))
+
+
+           
+    return app 
