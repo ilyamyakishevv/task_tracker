3879c561af1d58ccfca1577bb556739cf85597cb
ilyamyakishevv <112748839+ilyamyakishevv@users.noreply.github.com>
2022-10-08 11:33:29 +0300

    Feature/task add form (#4)
    * Start create a task_add_form
    
    * Add HTML template, add forms
    
    * HTML template some fix
    
    * Update with using flask context
    
    * added add task in DB, fix template using bootstrap, some changes
    
    * Form field updated for description field
    
    * Template  some update, adding date form

--- tasktrackerapp/models.py
+++ tasktrackerapp/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String, nullable=False)
-    desription = db.Column(db.String, nullable=False)
-    status = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, index=True, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False, default="OPEN")
     deadline = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
---
+++ tasktrackerapp/task_add_form.py
@@ -0,0 +1,12 @@
+from pydoc import render_doc
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class TaskAdd(FlaskForm):
+    name = StringField("Название задачи", validators=[DataRequired()], render_kw={'class':'form-control'})
+    description = TextAreaField("Описание задачи", validators=[DataRequired()], render_kw={'class':'form-control'})
+    status = StringField(default='OPEN', render_kw={'class':'form-control'})
+    deadline = DateField("Срок исполнения до", validators=[DataRequired()], render_kw={'class':'form-control'})
+    submit = SubmitField("Создать задачу", render_kw={'class':'btn btn-primary'})
