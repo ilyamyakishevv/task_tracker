0c18e80408797cfc8f99643bf11e2982d62b569c
Andrey Artemov <felix.brookman@gmail.com>
2022-10-13 22:09:56 +0300

    Добавлено 'Заопмнить меня' на странице логина

--- tasktrackerapp/forms.py
+++ tasktrackerapp/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField("Имя пользователя", validators=[DataRequired()], render_kw={"class": "form-control"})
     password = PasswordField("Пароль", validators=[DataRequired()],render_kw={"class": "form-control"})
     submit = SubmitField('Ввод', render_kw={"class":"btn btn-primary"})
-
+    remember = BooleanField('Запомнить меня', default=True, render_kw={"class":"form-check-input"})
     
 
 class AddForm(FlaskForm):
