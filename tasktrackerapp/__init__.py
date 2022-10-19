5cfbaf8546d6306f921443c01e2e86e81522d420
ilyamyakishevv <112748839+ilyamyakishevv@users.noreply.github.com>
2022-10-20 02:39:09 +0300

    Feature/edit and delete task (#10)
    * templates updated delete task func added
    
    * added edit and delete task func and templates

--- tasktrackerapp/__init__.py
+++ tasktrackerapp/__init__.py
@@ -5,7 +5,7 @@ from tasktrackerapp.forms import LoginForm, AddForm, DeleteForm
 from datetime import date
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
 
 
 
@@ -87,9 +87,37 @@ def create_app():
         return render_template('view_tasks.html', title=title, tasks=tasks) 
 
     @app.route('/task/<int:id>')
-    def task(id):
+    def get_task(id):
         task = Tasks.query.get(id)
         return render_template('task.html', task=task) 
+      
+    @app.route('/task/<int:id>/delete')
+    def delete_task(id):
+        task = Tasks.query.get_or_404(id)
+        try: 
+            db.session.delete(task)
+            db.session.commit()
+            return redirect(url_for('view_tasks'))
+        except:
+            return flash("При удалении задания произошла ошибка")
+
+    @app.route('/task/<int:id>/edit', methods=['POST', 'GET'])
+    def edit_task(id):
+        task = Tasks.query.get(id)
+        edit_form = TaskAdd()
+        if edit_form.validate_on_submit():
+            task.name = edit_form.name.data
+            task.description = edit_form.description.data
+            task.deadline = edit_form.deadline.data
+            if task.deadline < date.today():
+                flash("Ввелите корректную дату!")
+                return redirect(url_for('task_edit'))
+            db.session.commit()
+            flash("Задание успешно измненено")
+            return redirect(url_for('view_tasks'))
+        else: 
+            return render_template('edit_task.html', form=edit_form)
+
 
     @app.route('/login')
     def login():
