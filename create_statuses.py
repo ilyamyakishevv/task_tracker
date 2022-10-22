25b800a955ed07811ff6fd4590d37e720cf5bd1f
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-10-22 14:47:07 +0300

    changed create_statuses script using loop and dict

--- create_statuses.py
+++ create_statuses.py
@@ -1,29 +1,16 @@
-import sys
 from tasktrackerapp.models import Statuses, db
 from tasktrackerapp import create_app
 
 app = create_app()
 
 with app.app_context():
-    status_open = Statuses(
-                name="OPEN",
-                description="Статус используется когда задача только создана и не взята в работу"
-                )
-    status_in_work = Statuses(
-                name="IN WORK",
-                description="Статус используется когда задача взята в работу. но не выполнена"
-                )
-    status_in_review = Statuses(
-                name="IN REVIEW",
-                description="Статус используется когда задача выполнена и требует проверки"
-                )
-    status_done = Statuses(
-                name="DONE",
-                description="Статус используется когда задача выполнена и прошла проверку"
-                )
+    status_dict = {
+                "OPEN":"Статус используется когда задача только создана и не взята в работу",
+                "IN WORK":"Статус используется когда задача взята в работу. но не выполнена",
+                "IN REVIEW":"Статус используется когда задача выполнена и требует проверки",
+                "DONE":"Статус используется когда задача выполнена и прошла проверку"
+    }
     
-    db.session.add(status_open)
-    db.session.add(status_in_work)
-    db.session.add(status_in_review)
-    db.session.add(status_done)
+    for name in status_dict: 
+        db.session.add(Statuses(name=name, description=status_dict[name]))
     db.session.commit()
