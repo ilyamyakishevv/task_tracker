a07e8588dd88399c91f20dfa9041aa6f4e25265c
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-10-22 14:30:21 +0300

    Change script for adding statuses

--- create_statuses.py
+++ create_statuses.py
@@ -5,13 +5,25 @@ from tasktrackerapp import create_app
 app = create_app()
 
 with app.app_context():
-    status_name = input('Введите название статуса: ')
-    if Statuses.query.filter(Statuses.name == status_name).count():
-        print('Такой статус уже существует! Название статуса должно быть уникальным!')
-        sys.exit(0)
-    status_descriprion = input("Введите описание статуса (по желанию): ")
-
-    new_status = Statuses(name = status_name, description=status_descriprion)
-
-    db.session.add(new_status)
+    status_open = Statuses(
+                name="OPEN",
+                description="Статус используется когда задача только создана и не взята в работу"
+                )
+    status_in_work = Statuses(
+                name="IN WORK",
+                description="Статус используется когда задача взята в работу. но не выполнена"
+                )
+    status_in_review = Statuses(
+                name="IN REVIEW",
+                description="Статус используется когда задача выполнена и требует проверки"
+                )
+    status_done = Statuses(
+                name="DONE",
+                description="Статус используется когда задача выполнена и прошла проверку"
+                )
+    
+    db.session.add(status_open)
+    db.session.add(status_in_work)
+    db.session.add(status_in_review)
+    db.session.add(status_done)
     db.session.commit()
